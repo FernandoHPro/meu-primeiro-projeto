86d64c8c7dd4be7de49c0aec7c55cb63c6833bed
Fernando Henrique <fernando.cossa@projuris.com.br>
2021-07-01 11:34:44 -0300

    Versão inicial do projeto

---
+++ src/main/java/com/example/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,16 @@
+package com.example.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+//anotacao que prepara todo as dependencia
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+
+}
